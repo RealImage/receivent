115567e5e771b8669ae373536846bb654ce31dd6
Sudhir Jonathan <sudhir.j@gmail.com>
2020-02-20 12:18:16 +0530

    Added outline for HTTP receiver

---
+++ receivent.go
@@ -0,0 +1,37 @@
+package receivent
+
+import "encoding/json"
+import "net/http"
+
+type EventProcessor interface {
+	ProcessEvent(event json.RawMessage) error
+}
+
+type EventProcessorFunc func(event json.RawMessage) error
+
+func (f EventProcessorFunc) ProcessEvent(e json.RawMessage) error {
+	return f(e)
+}
+
+type Receiver struct {
+	processor EventProcessor
+}
+
+func (rc *Receiver) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	event := json.RawMessage{}
+
+	err := json.NewDecoder(r.Body).Decode(&event)
+	defer r.Body.Close()
+	if err != nil {
+		http.Error(w, "JSON decoding failed", http.StatusBadRequest)
+		return
+	}
+
+	err = rc.processor.ProcessEvent(event)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
